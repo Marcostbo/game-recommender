9a19ec1ed111fec8256d13106a48c517486c67db
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2022-08-02 20:52:53 -0300

    Calculate times

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from source.games import ReadGameFile
 from source.recommender import Recommender
+import time
+
+
+# STEP 1: Read file data
+start_time = time.time()
 
 path = r'C:\Users\Marcos Oliveira\PycharmProjects\game_recommender\data\twitch_game_data.csv'
 games = ReadGameFile()
 games.read_data(path=path)
+
+final_time = time.time()
+file_elapsed_time = final_time - start_time
+
+# STEP 2: Recommendations
+start_time = time.time()
 
 recommender = Recommender(game_data=games.data)
 recommender.create_tfidf_matrix()
@@ -12,9 +23,10 @@ recommender.create_cosine_similarities()
 desired_item_name = 'God of War'
 desired_item_id = games.data.loc[games.data['name'] == desired_item_name]['id'].values[0]
 
-recommender.get_game_by_id(desired_item_id)
-
 recommender.recommend(
     item_id=desired_item_id,
     number_of_recommendations=10
 )
+
+final_time = time.time()
+recommendation_elapsed_time = final_time - start_time
